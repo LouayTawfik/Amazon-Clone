2d48fe5d907de695da7c1e406a6992e89f8579b8
Louay Tawfik <codebreaker007@outlook.com>
2024-09-05 14:29:29 +0300

    add product reviews

--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product, Brand, ProductImages, Review
@@ -8,3 +9,8 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
     model = Product
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.filter(product=self.get_object())
+        return context
