dd84ac0215fa8d4e480b6e073a200c30eacd6f3a
Louay Tawfik <codebreaker007@outlook.com>
2024-09-11 13:53:45 +0300

    add product_list, detail api, serializer

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import BrandDetail, BrandList, ProductList, ProductDetail
+from .api import product_detail_api, product_list_api
 
 urlpatterns = [
     path('', ProductList.as_view()),
     path('<slug:slug>', ProductDetail.as_view()),
     path('brands/', BrandList.as_view()),
-    path('brands/<slug:slug>', BrandDetail.as_view())
+    path('brands/<slug:slug>', BrandDetail.as_view()),
+
+    # api
+    path('api/list', product_list_api),
+    path('api/detail/<int:product_id>', product_detail_api)
 ]
