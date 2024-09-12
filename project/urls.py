917e393dd6c977da44a015947a59031e5dc4c400
Louay Tawfik <codebreaker007@outlook.com>
2024-09-12 15:07:58 +0300

    add dj_rest_auth library

--- project/urls.py
+++ project/urls.py
@@ -28,8 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('product.urls')),
     path('', include('settings.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
 ] + debug_toolbar_urls()
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
