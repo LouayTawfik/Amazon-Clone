992b30bd73b297f7a1379771950342748d60e2fd
Louay Tawfik <codebreaker007@outlook.com>
2024-09-29 16:04:35 +0300

    show order details in orderlist page

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,15 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.db.models import Prefetch
+from django.views.generic import ListView
+from .models import Order, OrderDetail
 
-# Create your views here.
+
+class OrderListView(ListView):
+    model = Order
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset().prefetch_related(Prefetch('order_detail', queryset=OrderDetail.objects.select_related('product__brand'))).filter(user=self.request.user)
+        return queryset
