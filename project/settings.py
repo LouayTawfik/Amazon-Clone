199c8123817a290430d6c44e3b1594a218ea8600
Louay Tawfik <codebreaker007@outlook.com>
2025-01-14 02:44:50 +0200

    add swagger docs and session authentication

--- project/settings.py
+++ project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration',
     'rosetta',
     'django_extensions',
+    'drf_yasg',
 
     'product',
     'settings',
@@ -61,6 +62,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
